f4dbdbc31d2e2f47e72a475ae69941b1db97850c
Adaya-lew <adaya.lewartowski@post.runi.ac.il>
2024-01-17 21:44:55 +0200

    hw04

--- StringOps.java
+++ StringOps.java
@@ -33,10 +33,8 @@ public class StringOps {
             if(currentChar=='a' || currentChar=='i' || currentChar=='e' ||
              currentChar=='o' || currentChar=='u')  {
                 isSolution += upperCase(currentChar);
-            } else if (currentChar >= 'A' && currentChar <= 'Z') {
-            isSolution +=  lowerCase(currentChar);  
-        } else {
-            isSolution += currentChar; 
+            } else {
+            isSolution += lowerCase(currentChar); 
        }
 
    }
